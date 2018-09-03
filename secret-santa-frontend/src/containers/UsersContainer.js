fe84322ff309203b10b60bedc18ae889307adfff
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-09-03 17:07:03 -0500

    Created Users Container

---
+++ secret-santa-frontend/src/containers/UsersContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class UsersContainer extends Component {
+
+	render() {
+		return (
+			<div>
+				Create Account
+			</div>
+			)
+	}
+
+}
